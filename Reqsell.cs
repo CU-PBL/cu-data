5e1fcb7114186bc2dc9323aa401a0731c728fbbe
audzms2776 <diddydwns135@gmail.com>
2018-06-07 02:49:43 +0900

    Update Reqsell.cs

--- Reqsell.cs
+++ Reqsell.cs
@@ -1,62 +1,30 @@
-class Program 
+// 판매 기능
+static void CallAddSell()
 {
-    static void Main(string[] args)
+    var data = new SellData
     {
-        CallAddSell();
-    }
-        
-    // 판매 기능
-    static void CallAddSell()
-    {
-        var data = new SellData
-        {
-            PaymentOption = "bill",
-            CardNumber = 1635,
-            CashRecipt = "010-1234-5678",
-            Sum = 19000
-        };
+        paymentOption = "bill",
+        cardNumber = 1635,
+        cashRecipt = "010-1234-5678",
+        sum = 19000
+    };
 
-        var sellData = Cson<SellData>.Parse(data);
+    var sellData = Cson<SellData>.Parse(data);
 
-        var client = new RestClient("http://localhost:8000/sell");
-        var request = new RestRequest(Method.POST);
-        request.AddHeader("content-type", "application/json");
-        request.AddParameter("application/json", sellData, ParameterType.RequestBody);
-        var response = client.Execute(request);
+    var client = new RestClient("http://localhost:8000/sell");
+    var request = new RestRequest(Method.POST);
+    request.AddHeader("content-type", "application/json");
+    request.AddParameter("application/json", sellData, ParameterType.RequestBody);
+    var response = client.Execute(request);
 
-        Console.WriteLine(response.Content);
-    }
+    Console.WriteLine(response.Content);
+}
 
-    [DataContract]
-    class SellData
-    {
-        [DataMember] private string paymentOption;
-        [DataMember] private int cardNumber;
-        [DataMember] private string cashRecipt;
-        [DataMember] private int sum;
-
-        public string PaymentOption
-        {
-            get => paymentOption;
-            set => paymentOption = value;
-        }
-
-        public int CardNumber
-        {
-            get => cardNumber;
-            set => cardNumber = value;
-        }
-
-        public string CashRecipt
-        {
-            get => cashRecipt;
-            set => cashRecipt = value;
-        }
-
-        public int Sum
-        {
-            get => sum;
-            set => sum = value;
-        }
-    }
+[DataContract]
+class SellData
+{
+    [DataMember] public string paymentOption;
+    [DataMember] public int cardNumber;
+    [DataMember] public string cashRecipt;
+    [DataMember] public int sum;
 }
