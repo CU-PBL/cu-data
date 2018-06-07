7b2f6f4b2e00631620e8063472e3516ca10fe237
audzms2776 <diddydwns135@gmail.com>
2018-06-07 13:44:26 +0900

    Update Cson.cs

--- Cson.cs
+++ Cson.cs
@@ -1,25 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
 class Cson<T>
 {
     public static string Parse(T t)
     {
-        var stream1 = new MemoryStream();
-        var ser = new DataContractJsonSerializer(typeof(T));
-        ser.WriteObject(stream1, t);
-
-        stream1.Position = 0;
-        StreamReader sr = new StreamReader(stream1);
-
-        var strJson = sr.ReadToEnd();
-        return strJson;
+        return JsonConvert.SerializeObject(t);
     }
 
     public static T DeParse(string strData)
     {
-        var ser = new DataContractJsonSerializer(typeof(T));
-        var stream1 = new MemoryStream(Encoding.Unicode.GetBytes(strData)) { Position = 0 };
-        var p2 = (T)ser.ReadObject(stream1);
-
-        return p2;
+        return JsonConvert.DeserializeObject<T>(strData);
     }
 
     public static List<T> ArrParse(string mainData)
